0854db9dd743f4ef1f63b13db1c6ec155f0d5b5d
Munifatir Rizqi Aziz <munifatirra@gmail.com>
2024-03-13 13:22:27 +0700

    tipe data java

--- src/TipeDataNumber.java
+++ src/TipeDataNumber.java
@@ -2,16 +2,16 @@ public class TipeDataNumber {
 
     public static void main(String[] args) {
 
+        //angka defaultnya adalah 0
         byte byet1 = 10;
         short short1 = 1000;
         int int1 = 10000000;
         long long1 = 100000000;
         long long2 = 1000000000L;
-        //angka defaultnya adalah 0
 
+        // angka defaultnya adalah 0.0
         float float1 = 1.0f;
         double double1 = 1.0;
-        // angka defaultnya adalah 0.0
 
         int decInt = 50;
         int hexInt = 0xFFFFF;
